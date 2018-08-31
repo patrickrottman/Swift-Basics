bfbceb50e2c6675ca685f3ec63deb414ca79eab0
Patrick Rottman <scimi2000@gmail.com>
2018-08-31 13:29:58 -0500

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Patrick Rottman on 8/31/18.
+//  Copyright © 2018 Patrick Rottman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
